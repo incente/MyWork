d4cd38a5451419de06e46fc111f94a22fa8988b9
Vincentea <113775379+incente@users.noreply.github.com>
2022-10-13 17:05:03 +0200

    Console coded
    A button + function to switch the current mode 1-5. A spin button that just updates the wallet atm. And an input field  with a output that automaticly updates your input bet times your current mode.

---
+++ slots/slots.js
@@ -0,0 +1,35 @@
+let bet = 1;
+let currentbet = bet;
+let currentmode = 1;
+let wallet = 3000;
+
+const currentvalue = () => {
+    bet = document.getElementById("bet-input").value;
+    currentbet = currentmode * bet;
+    document.getElementById("current-bet").innerHTML = 
+        currentbet + " USD";
+}
+
+const changemode = () => {
+    if (currentmode == 5)   {
+        currentmode -= 4;
+    } else {
+        currentmode++
+    }
+    document.getElementById("current-mode").innerHTML =
+        currentmode;
+    document.getElementById("multi").innerHTML = 
+        currentmode + "x";
+    currentbet = currentmode * bet;
+    document.getElementById("current-bet").innerHTML = 
+        currentbet + " USD";
+}
+
+const spin = () => {
+    wallet -= currentbet;
+    document.getElementById("wallet").innerHTML =
+        wallet + " USD";
+    
+}
+
+
