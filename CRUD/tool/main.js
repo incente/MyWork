4626cd8de1780cbd9b9eb8445cb925fd52fc93c1
Vincentea <113775379+incente@users.noreply.github.com>
2022-11-17 03:14:54 +0100

    cleaned

--- CRUD/tool/main.js
+++ CRUD/tool/main.js
@@ -3,6 +3,8 @@ let textInput = document.getElementById('textInput');
 let dateInput = document.getElementById('dateInput');
 let textarea = document.getElementById('textarea');
 let msg = document.getElementById('msg');
+let tasks = document.getElementById('tasks');
+let add = document.getElementById('add');
 
 form.addEventListener('submit', (e) =>  {
     e.preventDefault();
@@ -11,21 +13,76 @@ form.addEventListener('submit', (e) =>  {
 
 let formValidaiton = () => {
     if (textInput.value === ''){
-        console.log('faliure');
         msg.innerHTML = "Task cannot be blank"
     } else {
-        console.log('success');
         msg.innerHTML = "";
         acceptdata();
+        add.setAttribute('data-bs-dismiss', 'modal');
+        add.click();
+
+        (() => {
+            add.setAttribute('data-bs-dismiss', 'modal');
+        })();
     }
 };
 
-let data = {};
+let data = [];
 
 let acceptdata = () => {
-    data['text'] = textInput.value;
-    data['date'] = dateInput.value;
-    data['description'] = textarea.value;
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textarea.value
+    });
 
-    console.log(data);
+    localStorage.setItem('data', JSON.stringify(data));
+
+    createTasks();
 };
+
+let createTasks = () => {
+    tasks.innerHTML = '';
+    data.map((x, y) => {
+        return (tasks.innerHTML += `
+        <div id=${y}>
+          <span class='fw-bold'>${x.text}</span>
+          <span class='fw-bold text-secondary'>${x.date}</span>
+          <p>${x.description}</p>
+    
+          <span class="options">
+            <i onClick="editTask(this)" data-bs-toggle='modal' data-bs-target='#form' class="fas fa-edit"></i>
+            <i onClick="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+        `);
+    })
+    
+    resetForm();
+};
+
+let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem('data', JSON.stringify(data));
+};
+
+let editTask = (e) => {
+    let selectedTask = e.parentElement.parentElement;
+
+    textInput.value = selectedTask.children[0].innerHTML;
+    dateInput.value = selectedTask.children[1].innerHTML;
+    textarea.value = selectedTask.children[2].innerHTML;
+
+    deleteTask(e);
+};
+
+let resetForm = () => {
+    textInput.value = '';
+    dateInput.value = '';
+    textarea.value = '';
+};
+
+(() => {
+    data = JSON.parse(localStorage.getItem('data')) || [];
+    createTasks();
+})();
