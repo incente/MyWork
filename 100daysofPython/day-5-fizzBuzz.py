73654fd39a09cbd2d1d73e8b8c89cbfe48a95143
Vincentea <113775379+incente@users.noreply.github.com>
2022-12-12 22:31:32 +0100

    day 6

--- 100daysofPython/day-5-fizzBuzz.py
+++ 100daysofPython/day-5-fizzBuzz.py
@@ -10,7 +10,6 @@ for n in range(1, 101):
         output = "Buzz"
     else:
         output = n
-    print(output)
 
 #1.2 Password generator
 all_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -22,30 +21,26 @@ letters = int(input("How many letters would you like in your password?\n"))
 symbols = int(input("How many symbols would you like?\n"))
 digits = int(input("How many numbers would you like?\n"))
 
-password_sort = ""
+passwordList = []
+password = []
 
 for n in range(0, letters):
-    password_sort += all_letters[random.randint(0, len(all_letters) - 1)]
+    rand = random.randint(0, len(all_letters) - 1)
+    passwordList.append(all_letters[rand])
 
 for n in range(0, symbols):
-    password_sort += all_symbols[random.randint(0, len(all_symbols) - 1)]
+    rand = random.randint(0, len(all_symbols) - 1)
+    passwordList.append(all_symbols[rand])
 
 for n in range(0, digits):
-    password_sort += all_digits[random.randint(0, len(all_digits) - 1)]
+    rand = random.randint(0, len(all_digits) - 1)
+    passwordList.append(all_digits[rand])
 
-password = ""
-old_order = []
-new_order = []
+for n in range(0, len(passwordList) - 1):
+    rand = random.randint(0, len(passwordList) - 1)
+    password.append(passwordList[rand])
+    passwordList.pop(rand)
 
-for n in range(0, len(password_sort) - 1):
-    old_order.append(n)
-
-print(old_order)
-
-for n in range(0, len(password_sort)):
-    rand = random.randint(0, len(old_order) - 1)
-    print(rand)
-    new_order.append(old_order[rand])
-    old_order.pop(rand)
-
-print(new_order)
+print(passwordList)
+print(password)
+print(len(password))
---
+++ 100daysofPython/day-6-functions.py
@@ -0,0 +1,18 @@
+#1. Functions
+def my_function(n):
+    print(n)
+
+my_function("This is a function!")
+
+#1.1 Tabs or spaces?
+    #Setting -> Tab size -> 4 space
+
+#2. While loop
+five = 0
+is_five = False
+
+while not is_five:
+    five += 1
+    if five == 5:
+        is_five = True
+    print(is_five)
