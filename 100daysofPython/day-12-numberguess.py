38e4b7e08703252ac5cf885c94e3517298d8a14a
Vincentea <113775379+incente@users.noreply.github.com>
2022-12-21 15:51:30 +0100

    Guess number game

--- 100daysofPython/day-12-numberguess.py
+++ 100daysofPython/day-12-numberguess.py
@@ -2,32 +2,69 @@ import random
 
 def create_num():
 
-    def create_min():
+    while True:
+
         try:
-            min = int(input("Min: "))
-            return min
+            range_min = int(input("Min: "))
+            break
 
         except ValueError:
             print("Minimum must be an integer")
-            create_min()
         
-    def create_max():
+    while True:
+
         try:
-            max = int(input("Max: "))
-            return max
+            range_max = int(input("Max: "))
+            break
 
         except ValueError:
             print("Maximum must be an integer")
-            create_max()
 
-    num = random.randint(create_min(), create_max())
-    print(num)
-    return num
+    num = random.randint(range_min, range_max)
+    print(num) #little hint
+    return num, range_min, range_max
 
-def guess():
-    while guess != create_num():
-        guess = input("Guess: ")
+def guess(range_min, range_max):
 
-def check(x, y):
-    if x == y:
-        print(f"You guessed {x}, thats the right number")
+    while True:
+
+        guess_num = input("Guess a number, type 'give up' if you want to end: ")
+
+        if guess_num == "give up":
+            print("Goodbye")
+            break
+        else:
+            try:
+                guess_num = int(guess_num)
+
+                if guess_num < range_min:
+                    print(f"You guessed {guess_num}, thats below the min of {range_min}")
+                elif guess_num > range_max:
+                    print(f"You guessed {guess_num}, thats above the max of {range_max}")
+                else:
+                    return guess_num
+
+            except ValueError:
+                    print("Guess must be an integer")
+        
+def check(guess_num, num, range_min, range_max):
+    counter = 1
+    while True:
+        if guess_num == num:
+            print(f"You guessed {guess_num}, thats the right number. Total guesses: {counter}")
+            again = input("Type 'yes' if you want to go again? ")
+            if again.lower() == "yes":
+                start()
+            else:
+                break
+            
+        else:
+            print(f"You guessed {guess_num}, thats the wrong number. Guesses: {counter}")
+            guess_num = guess(range_min, range_max)
+            counter += 1
+            
+def start():
+    num, range_min, range_max = create_num()
+    check(guess(range_min, range_max), num, range_min, range_max)
+
+start()
